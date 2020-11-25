99ca0a35f63c159c6198574b8d0bf4fbf4e9311d
Rahul Ranjan <rahul.ranjan6344@gmail.com>
2020-11-25 16:16:01 +0530

    sdf

--- src/components/page2.js
+++ src/components/page2.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './second.css'
 import Grid from '@material-ui/core/Grid';
 import fifty from '../assets/fifty.svg'
+import learn from '../assets/learn.svg'
+import anuradha from '../assets/anuradha.svg'
 const SecondPage=()=>{
     return(
         <div className={'second'}>
-            <Grid container justify={"center"}>
+            <Grid container justify={"center"} lg={10} justify={"space-around"}>
                 <Grid item lg={4}>
                     <img src={fifty} height={150} alt=""/>
                 </Grid>
@@ -14,6 +16,32 @@ const SecondPage=()=>{
                         by learning with the help of Augmented Reality</p>
                     <p style={{color:'black',textAlign:'right'}}>~NCERT</p>
                 </Grid>
+                <Grid item  lg={10} style={{position:'absolute',top:'28%',left:'5%'}}>
+                    <img src={learn}  alt=""/>
+                </Grid>
+                <Grid item container style={{position:'absolute',top:'60%',color:'black'}} lg={10}>
+                    <Grid item  lg={7} >
+                        <img src={anuradha} height={400}  alt=""/>
+                    </Grid>
+                    <Grid item  lg={5}
+
+                    >
+                        <div>
+                            <h3>Anuradha Mathur</h3>
+                            <span>HOD-Kalam Labs</span>
+                            <p>Mrs Anuradha Madhur is one of the most celebrated Physisicts in India.
+                                Core authors for Physics NCERT Textbooks of Class 11th and 12th.
+                                She has
+                                previously taught at Harvard University, USA
+                                Ex- HOD Physics NCERT.
+                            </p>
+                            <p>At Kalam Labs, she guides students across Globe to Study Science Experimentally.</p>
+                        </div>
+
+                    </Grid>
+                </Grid>
+
+
             </Grid>
 
         </div>
